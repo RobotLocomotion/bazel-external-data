1dd37eb48f5cbae47efe13fffc564a4504f34fa8
Eric Cousineau <eric.cousineau@tri.global>
2019-01-07 12:39:21 -0500

    Minor cleanup

--- bazel_external_data/backends/test/girder_test_runner.sh
+++ bazel_external_data/backends/test/girder_test_runner.sh
@@ -3,10 +3,13 @@ set -eux -o pipefail
 
 cd $(dirname $0)
 
+docker_log=/tmp/docker_girder.output.txt
+
 (
     cd docker_girder
     docker-compose build
-    docker-compose up &
+    echo "docker-compose output: ${docker_log}"
+    docker-compose up > ${docker_log} 2>&1 &
     # TODO: Use `wait-for`.
     sleep 5
     # Setup server.
@@ -17,3 +20,6 @@ cd $(dirname $0)
 bazel build //bazel_external_data/backends:girder_test
 ws=$(bazel info workspace)
 ${ws}/bazel-bin/bazel_external_data/backends/girder_test ./docker_girder/build/info.yml
+
+# Shutdown.
+docker-compose down
