afaa530ba96eae5386b272bc781e1dff8068528a
Eric Cousineau <eric.cousineau@tri.global>
2022-02-23 18:35:21 -0500

    Add external_data_repository_download (#8)
    Drop cli_extra_args for simplicity (no longer any concrete use case)

---
+++ .external_data_cli_proxy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Provides a mechanism to expose //:cli` functionality to leverage external
+# data in repository rules.
+import os
+import sys
+
+src_dir = os.path.dirname(os.readlink(os.path.abspath(__file__)))
+# Trace back to source workspace so we can use existing source.
+env = dict(os.environ)
+env["PYTHONPATH"] = src_dir + ":" + env.get("PYTHONPATH", "")
+args = [sys.executable, "-m", "bazel_external_data.cli"] + sys.argv[1:]
+os.execve(args[0], args, env)
